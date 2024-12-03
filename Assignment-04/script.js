491a0f49ed7f073ff1aa3fce96581d744a3c2062
vedant hatte <vedant.hatte@haptiq.com>
2024-12-03 19:07:49 +0530

    changes done

--- Assignment-04/script.js
+++ Assignment-04/script.js
@@ -1,38 +1,10 @@
-// $(document).ready(function () {
-//     // Initialize the slider for the categories section
-//     $('.categoriesSlider').slick({
-//         infinite: true,
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         arrows: true, // Adds navigation arrows
-//         prevArrow: '<button class="slick-prev"> ❮ </button>',
-//         nextArrow: '<button class="slick-next"> ❯ </button>',
-//         responsive: [
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 3,
-//                     slidesToScroll: 1,
-//                 },
-//             },
-//             {
-//                 breakpoint: 480,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 1,
-//                 },
-//             },
-//         ],
-//     });
-// });
-
 $(document).ready(function () {
     $('.categoriesLists').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        prevArrow: '<button class="slick-prev"><span class="custom-prev-arrow"><img src="./IMAGES/left arrow.png" width="12" height="24" alt="prevArrow"></span></button>',
-        nextArrow: '<button class="slick-next"><span class="custom-next-arrow"><img src="./IMAGES/right arrow.png" width="12" height="24" alt="nextArrow"></span></button>',
+        prevArrow: '<button class="slickPrev"><span class="custom-prev-arrow"><img src="./IMAGES/left arrow.png" width="12" height="24" alt="prevArrow"></span></button>',
+        nextArrow: '<button class="slickNext"><span class="custom-next-arrow"><img src="./IMAGES/right arrow.png" width="12" height="24" alt="nextArrow"></span></button>',
         arrows: true,
 
         responsive: [
@@ -58,8 +30,26 @@ $(document).ready(function () {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        prevArrow: '<button class="slick-prev"><span class="custom-prev-arrow"><img src="./IMAGES/left arrow.png" width="12" height="24" alt="prevArrow"></span></button>',
-        nextArrow: '<button class="slick-next"><span class="custom-next-arrow"><img src="./IMAGES/right arrow.png" width="12" height="24" alt="nextArrow"></span></button>',
+
+        prevArrow: '<button class="slickPrev"><span class="custom-prev-arrow"><img src="./IMAGES/left arrow.png" width="12" height="24" alt="prevArrow"></span></button>',
+        nextArrow: '<button class="slickNext"><span class="custom-next-arrow"><img src="./IMAGES/right arrow.png" width="12" height="24" alt="nextArrow"></span></button>',
         arrows: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            }
+        ]
     });
 });
